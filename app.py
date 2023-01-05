22ded03331a14e8ff3c2a2fc8eb4fa906851952a
ymichael14 <ym88163@gmail.com>
2023-01-04 20:35:29 -0600

    added paths to app.py and changed paths to each html file

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def echo():
     return render_template('index.html')
 
+@app.route('/playlist')
+def playlist():
+    return render_template('playlist.html')
+
+@app.route('/features')
+def features():
+    return render_template('features.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
 #debugger to edit while running
 if __name__ == "__main__":
     app.run(debug=True)
